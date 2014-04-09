bc0e1c8d04315c9dae2ebbb3a4396e98ac1d9e8d
Peter Zijlstra <peterz@infradead.org>
2014-04-09 15:35:08 +0200

    sched/idle: Avoid spurious wakeup IPIs
    Because mwait_idle_with_hints() gets called from !idle context it must
    call current_clr_polling(). This however means that resched_task() is
    very likely to send an IPI even when we were polling:
    
      CPU0					CPU1
    
      if (current_set_polling_and_test())
        goto out;
    
      __monitor(&ti->flags);
      if (!need_resched())
        __mwait(eax, ecx);
    					set_tsk_need_resched(p);
    					smp_mb();
    out:
      current_clr_polling();
    					if (!tsk_is_polling(p))
    					  smp_send_reschedule(cpu);
    
    So while it is correct (extra IPIs aren't a problem, whereas a missed
    IPI would be) it is a performance problem (for some).
    
    Avoid this issue by using fetch_or() to atomically set NEED_RESCHED
    and test if POLLING_NRFLAG is set.
    
    Since a CPU stuck in mwait is unlikely to modify the flags word,
    contention on the cmpxchg is unlikely and thus we should mostly
    succeed in a single go.
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Acked-by: Nicolas Pitre <nico@linaro.org>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: http://lkml.kernel.org/n/tip-kf5suce6njh5xf5d3od13rr0@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -657,15 +657,14 @@ void resched_task(struct task_struct *p)
 	if (test_tsk_need_resched(p))
 		return;
 
-	set_tsk_need_resched(p);
-
 	cpu = task_cpu(p);
-	if (cpu == smp_processor_id())
-		return;
 
-	/* NEED_RESCHED must be visible before we test polling */
-	smp_mb();
-	if (!tsk_is_polling(p))
+	if (cpu == smp_processor_id()) {
+		set_tsk_need_resched(p);
+		return;
+	}
+
+	if (set_nr_and_not_polling(p))
 		smp_send_reschedule(cpu);
 }
 
