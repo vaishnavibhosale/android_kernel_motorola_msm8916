de43b9ca3412f62f0add6de61e93094944d4476c
Shiju Mathew <shijum@codeaurora.org>
2015-01-08 20:06:55 -0500

    power: bcl: Add info logs for BCL mode and hotplug updates
    This patch will add info logs when there is any update
    to bcl mode and hotplug masks. This is required since
    this is the only way to find out if these critical
    features are updated for a short period of time such
    as during booting.
    
    Change-Id: Ibc00d3e3a4e2c5ce971c7777a4a7dc0386d26da6
    Signed-off-by: Shiju Mathew <shijum@codeaurora.org>

--- drivers/power/battery_current_limit.c
+++ drivers/power/battery_current_limit.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -620,12 +620,16 @@ mode_store(struct device *dev, struct device_attribute *attr,
 		pr_err("No gbcl pointer\n");
 		return -EPERM;
 	}
-	if (!strncmp(buf, "enable", 6))
+	if (!strncmp(buf, "enable", 6)) {
 		bcl_mode_set(BCL_DEVICE_ENABLED);
-	else if (!strncmp(buf, "disable", 7))
+		pr_info("bcl enabled\n");
+	} else if (!strncmp(buf, "disable", 7)) {
 		bcl_mode_set(BCL_DEVICE_DISABLED);
-	else
+		pr_info("bcl disabled\n");
+	} else {
 		return -EINVAL;
+	}
+
 	return count;
 }
 
@@ -903,6 +907,7 @@ static ssize_t hotplug_mask_store(struct device *dev,
 		return ret;
 
 	bcl_hotplug_mask = val;
+	pr_info("bcl hotplug mask updated to %d\n", bcl_hotplug_mask);
 
 	return count;
 }
@@ -921,6 +926,7 @@ static ssize_t hotplug_soc_mask_store(struct device *dev,
 		return ret;
 
 	bcl_soc_hotplug_mask = val;
+	pr_info("bcl soc hotplug mask updated to %d\n", bcl_soc_hotplug_mask);
 
 	return count;
 }
